20e75ad0510415885d86a380bce2009c027a13d1
Futaba-Kosuke <k.futaba2828@gmail.com>
2020-05-29 14:10:20 +0900

    Add: 課題5を追加

---
+++ 0528/server.py
@@ -0,0 +1,29 @@
+import socket
+
+# 1. ソケットの作成
+"""
+第4層: TCP
+第3層: IPv4
+"""
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# 2. ソケットへ自分の情報を登録
+sock.bind(("", 50000))
+
+# 3. 誰かの接続を待つ
+sock.listen()
+
+# 4. 接続要求に応える
+sock_c, addr = sock.accept()
+
+msg = '4433'
+
+try:
+    sock_c.sendall(msg.encode('utf-8'))
+except:
+    print('sendall function failed.')
+
+# 5. ソケットを閉じる
+sock_c.close()
+sock.close()
+
